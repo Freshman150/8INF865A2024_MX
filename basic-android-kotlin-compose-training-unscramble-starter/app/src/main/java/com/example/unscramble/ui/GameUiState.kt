f4a876169de35225f30b110a3ce6339fc52db957
Maxime <maxime.xxu@gmail.com>
2024-10-29 11:58:22 -0400

    Etape 7

--- basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -1,3 +1,4 @@
 data class GameUiState(
-    val currentScrambledWord: String = ""
+    val currentScrambledWord: String = "",
+    val isGuessedWordWrong: Boolean = false,
 )
--- basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -6,6 +6,7 @@ import com.example.unscramble.data.allWords
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import kotlinx.coroutines.flow.update
 
 class GameViewModel : ViewModel() {
 
@@ -47,6 +48,20 @@ class GameViewModel : ViewModel() {
 
     fun updateUserGuess(guessedWord: String){
         userGuess = guessedWord
+    }
+
+    fun checkUserGuess() {
+        if (userGuess.equals(currentWord, ignoreCase = true)) {
+
+        }
+        else {
+            // User's guess is wrong, show an error
+            _uiState.update { currentState ->
+                currentState.copy(isGuessedWordWrong = true)
+            }
+        }
+        // Reset user guess
+        updateUserGuess("")
     }
 
     init {
